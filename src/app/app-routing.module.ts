98a19f15a3255fc9868baa82b1f632823a45d1e5
ธนากร ดวงแก้ว <thanagorn26@gmail.com>
2020-12-14 11:28:49 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+// import { AuthGuard } from './_guards/auth.guard';
+// import { AuthLoginGuard } from './_guards/auth-login.guard';
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/login', pathMatch: 'full'
+  },
+  {
+    path: 'login', component: LoginComponent
+    // , canActivate: [AuthLoginGuard] 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+
